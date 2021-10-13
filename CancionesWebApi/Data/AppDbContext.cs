5537c4305365c47b322fffaf0bed6b5acc69328a
Espero pueda subirlo <a2020111314@estudiantes.upsa.edu.bo>
2021-10-13 14:24:00 -0400

    AppDBContext creado

---
+++ CancionesWebApi/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CancionesWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CancionesWebApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        // Agregar los modelos a utilizar
+        public DbSet<Canciones> Canciones { get; set; }
+    }
+}
